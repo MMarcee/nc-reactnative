2e7b7ad6c0c00144b24012b99dca514727582c12
MMarcee <marbouame@gmail.com>
2021-03-26 14:32:39 -0400

    Local Notifications

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -3,6 +3,7 @@ import { Text, View, ScrollView, StyleSheet,
     Picker, Switch, Button, Alert } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import * as Animatable from 'react-native-animatable';
+import * as Notifications from 'expo-notifications';
 
 class Reservation extends Component {
 
@@ -31,12 +32,15 @@ class Reservation extends Component {
           onPress: () => this.resetForm(),
           style: "cancel"
         },
-        { text: "OK", onPress: () => this.resetForm()}
+        { text: "OK", 
+        onPress: () => {
+            this.presentLocalNotification(this.state.date.toLocaleDateString('en-US'));
+            this.resetForm();
+            }
+        }
       ]
     );
-    // toggleModal() {
-    //     this.setState({showModal: !this.state.showModal});
-    // }
+    
 
      handleReservation() {
          console.log(JSON.stringify(this.state));
@@ -49,8 +53,32 @@ class Reservation extends Component {
             hikeIn: false,
             date: new Date(),
             showCalendar: false,
-            //showModal: false  
         });
+    }
+    async presentLocalNotification(date) {
+        function sendNotification() {
+            Notifications.setNotificationHandler({
+                handleNotification: async () => ({
+                    shouldShowAlert: true
+                })
+            });
+
+            Notifications.scheduleNotificationAsync({
+                content: {
+                    title: 'Your Campsite Reservation Search',
+                    body: `Search for ${date} requested`
+                },
+                trigger: null
+            });
+        }
+
+        let permissions = await Notifications.getPermissionsAsync();
+        if (!permissions.granted) {
+            permissions = await Notifications.requestPermissionsAsync();
+        }
+        if (permissions.granted) {
+            sendNotification();
+        }
     }
 
     render() {
